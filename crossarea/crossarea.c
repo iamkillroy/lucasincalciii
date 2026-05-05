b943c17f00c881bd841215764164929afdb1de93
iamkillroy <lucasfrias@mailfence.com>
2026-05-04 19:52:56 -0500

    Created cross area with based vector struct

---
+++ crossarea/crossarea.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct vector_struct {
+    int x;
+    int y;
+    int z;
+};
+typedef struct vector_struct Vec;
+
+
+int get_2_by_2_det(int a, int b, int c, int d){
+
+}
+
+int get_cross_product(){
+
+}
+
+
+int get_magnitude(){
+
+}
+
+int get_cross_area(){
+
+    return 0;
+}
+
+
+int main(){
+
+}
